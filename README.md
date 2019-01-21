73eb7791bb2992ca238219c7557a707441bf0ac8
zhaiyu <zishuzy@qq.com>
2019-01-21 10:53:36 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+# logrus日志按照时间分割
+
+[参考](https://xiaodongliu.com/2017/12/11/logrus%E6%97%A5%E5%BF%97%E6%8C%89%E7%85%A7%E6%97%B6%E9%97%B4%E5%88%86%E5%89%B2/index.html)
---
+++ zylog.go
@@ -0,0 +1,54 @@
+package zylog
+
+import (
+	"github.com/lestrrat/go-file-rotatelogs"
+	"github.com/pkg/errors"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
+	"log"
+	"os"
+	"path"
+	"time"
+)
+
+var Log *logrus.Logger
+
+func init() {
+	path := "./log/"
+	initPath(path)
+	Log = logrus.New()
+	Log.SetLevel(logrus.InfoLevel)
+	ConfigLocalFilesystemLogger("", path+"std.log", time.Hour*24, time.Second*20)
+}
+
+func initPath(path string) {
+	if _, err := os.Stat(path); err != nil {
+		err := os.MkdirAll(path, 0755)
+		if err != nil {
+			log.Println("Error creating directory,", err)
+		}
+	}
+}
+
+func ConfigLocalFilesystemLogger(logPath string, logFileName string, maxAge time.Duration, rotationTime time.Duration) {
+	baseLogPaht := path.Join(logPath, logFileName)
+	writer, err := rotatelogs.New(
+		baseLogPaht+".%Y%m%d%H%M%S",
+		//rotatelogs.WithLinkName(baseLogPaht),      // 生成软链，指向最新日志文件
+		rotatelogs.WithMaxAge(maxAge),             // 文件最大保存时间
+		rotatelogs.WithRotationTime(rotationTime), // 日志切割时间间隔
+	)
+	if err != nil {
+		Log.Errorf("config local file system logger error. %+v", errors.WithStack(err))
+	}
+	lfHook := lfshook.NewHook(lfshook.WriterMap{
+		logrus.DebugLevel: writer, // 为不同级别设置不同的输出目的
+		logrus.InfoLevel:  writer,
+		logrus.WarnLevel:  writer,
+		logrus.ErrorLevel: writer,
+		logrus.FatalLevel: writer,
+		logrus.PanicLevel: writer,
+	}, &logrus.TextFormatter{DisableColors: true, TimestampFormat: "2006-01-02 15:04:05.000"})
+	//注意上面这个 and &amp;符号被转义了
+	Log.AddHook(lfHook)
+}
